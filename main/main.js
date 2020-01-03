95273da0ff065f53e0211c15baf4dfe7f788d894
Rohr <krohr@krohrMBP.fullsail.com>
2020-01-03 14:41:05 -0500

    HTML structure and beginning of form

---
+++ main/main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    let twoPlus = 5/6;
+    let threePlus = 4/6;
+    let fourPlus = 3/6;
+    let fivePlus = 2/6;
+    let sixPlus = 1/6;
+
+    // console.log("Roll 40 dice looking for 4 up.")
+    // let diceCount = 40;
+    // let result = diceCount * fourPlus;
+    // console.log("Out of " + diceCount + " dice, " + result + " were successes.");
+
+
+})
