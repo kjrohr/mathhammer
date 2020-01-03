34c8d21e1b721545d3a4711215ba49d8ffc753ad
Rohr <krohr@krohrMBP.fullsail.com>
2020-01-03 16:15:40 -0500

    Added more form value capture

--- main/main.js
+++ main/main.js
@@ -10,18 +10,62 @@ document.addEventListener("DOMContentLoaded",()=>{
   
     submit.addEventListener("click",(e)=>{
         e.preventDefault();
+        console.clear();
         // console.log("asdf");
         let diceCount = document.getElementById("diceCount").value;
         console.log("Dice Count: " + diceCount);
-        let toHit = document.getElementsByName("hit");
-        for (let i = 0; i < toHit.length;i++)
+        let toHitOptions = document.getElementsByName("hit");
+        let toWoundOptions = document.getElementsByName("wound");
+        let rendOptions = document.getElementsByName("rend");
+        let armorOptions = document.getElementsByName("armor");
+        let damage = document.getElementById("totalDamage").value;
+        let toHit = 2;
+        let toWound = 2;
+        let rend = 0;
+        let armor = 2;
+
+        for (let i = 0; i < toHitOptions.length;i++)
         {
-            if(toHit[i].checked)
+            if(toHitOptions[i].checked)
             {
-                console.log("To Hit: " + toHit[i].value);
+                console.log("To Hit: " + toHitOptions[i].value);
+                toHit = toHitOptions[i].value;
                 break;
             }
         }
+
+        for (let i = 0; i < toWoundOptions.length;i++)
+        {
+            if(toWoundOptions[i].checked)
+            {
+                console.log("To Wound: " + toWoundOptions[i].value);
+                toWound = toWoundOptions[i].value;
+                break;
+            }
+        }
+
+        for (let i = 0; i < rendOptions.length;i++)
+        {
+            if (rendOptions[i].checked)
+            {
+                console.log("Rend: " + rendOptions[i].value);
+                rend = rendOptions[i].value;
+                break;
+            }
+        }
+
+        for (let i = 0; i < armorOptions.length; i++)
+        {
+            if (armorOptions[i].checked)
+            {
+                console.log("Target Armor: " + armorOptions[i].value);
+                armor = armorOptions[i].value;
+                break;
+            }
+        }
+        
+        console.log("Damage per attack: " + totalDamage);
+
     });
 
     // console.log("Roll 40 dice looking for 4 up.")
