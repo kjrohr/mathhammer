7c1a000bfe9a53ea05db3c6344cf6fe8e104108c
Rohr <krohr@krohrMBP.fullsail.com>
2020-01-03 15:54:49 -0500

    Event listener on b utton

--- main/main.js
+++ main/main.js
@@ -5,6 +5,14 @@ document.addEventListener("DOMContentLoaded",()=>{
     let fivePlus = 2/6;
     let sixPlus = 1/6;
 
+    let submit = document.getElementById("btnSubmit");
+    
+  
+    submit.addEventListener("click",(e)=>{
+        e.preventDefault();
+        console.log("asdf");
+    });
+
     // console.log("Roll 40 dice looking for 4 up.")
     // let diceCount = 40;
     // let result = diceCount * fourPlus;
@@ -12,3 +20,9 @@ document.addEventListener("DOMContentLoaded",()=>{
 
 
 })
+
+
+function test()
+{
+    console.log("caught");
+}
